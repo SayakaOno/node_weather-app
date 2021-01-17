5d9a1e13d6b872025028773ec513b8caba17f692
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-16 18:37:14 -0800

    implement map API call

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const request = require('request');
-const url = require('./apiURL');
+const { weatherUrl, mapUrl } = require('./apiURL');
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: weatherUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to weather service!');
+  } else if (response.body.error) {
+    console.log('Unable to find location');
+  } else {
+    const {
+      weather_descriptions,
+      temperature,
+      feelslike
+    } = response.body.current;
+    console.log(
+      `${
+        weather_descriptions[0]
+      }. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`
+    );
+  }
+});
+
+request({ url: mapUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to location services!');
+  } else if (!response.body.features.length) {
+    console.log('Unable to find location. Try another search.');
+  } else {
+    const [longitude, latitude] = response.body.features[0].center;
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  }
 });
